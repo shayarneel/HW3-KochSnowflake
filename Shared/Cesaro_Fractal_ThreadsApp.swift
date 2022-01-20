d39cdeec38f418c91b6d4cbd55004153c190ca61
jterry94 <terryj@iit.edu>
2022-01-19 20:07:39 -0600

    Initial Commit

---
+++ Shared/Cesaro_Fractal_ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cesaro_Fractal_ThreadsApp.swift
+//  Shared
+//
+//  Created by Jeff_Terry on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Cesaro_Fractal_ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
